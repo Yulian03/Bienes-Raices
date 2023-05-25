3e77263865430878cce03ab59a36ec4f353593ef
Paola Villalobos <yulian_yuratma0328@hotmail.com>
2023-05-24 21:49:25 -0500

    agregando formulario de contacto

---
+++ src/js/app.js
@@ -0,0 +1,30 @@
+document.addEventListener ('DOMContentLoaded', function() {
+    
+    eventListener ();  
+    
+    darkMode ();
+});
+
+function darkMode() {
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+
+    botonDarkMode.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+    });
+}
+
+function eventListener (){
+   const mobileMenu = document.querySelector('.mobile-menu');
+
+   mobileMenu.addEventListener ('click', navegacionResponsive);
+}
+
+function navegacionResponsive (){
+    const navegacion = document.querySelector('.navegacion');
+
+    if (navegacion.classList.contains('mostrar')) {
+        navegacion.classList.remove('mostrar');
+    } else {
+        navegacion.classList.add('mostrar');
+    }
+}
